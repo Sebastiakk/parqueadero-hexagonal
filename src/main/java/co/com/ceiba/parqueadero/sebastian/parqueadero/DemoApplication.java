f76719f2b463439f422d42871f8180c91b46e413
Sebastian <sebastianramirezherrera65@gmal.com>
2019-06-25 08:56:28 -0500

    Primer commit con la estructura de ceiba

---
+++ src/main/java/co/com/ceiba/parqueadero/sebastian/parqueadero/DemoApplication.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.parqueadero.sebastian.parqueadero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
