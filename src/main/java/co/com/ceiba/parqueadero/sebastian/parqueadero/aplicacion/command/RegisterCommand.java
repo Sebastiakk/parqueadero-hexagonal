fb519436664220ee139355b7428b161b57dc5228
Sebastian <sebastianramirezherrera65@gmal.com>
2019-06-27 09:33:02 -0500

    Capa de aplicaciones

---
+++ src/main/java/co/com/ceiba/parqueadero/sebastian/parqueadero/aplicacion/command/RegisterCommand.java
@@ -0,0 +1,63 @@
+package co.com.ceiba.parqueadero.sebastian.parqueadero.aplicacion.command;
+
+import java.util.Date;
+
+public class RegisterCommand {
+        private long idCupo;
+        private String placa;
+        private Date horaEntrada;
+        private Date horaSalida;
+        private String tipoVehiculo;
+
+        public RegisterCommand() {
+        }
+
+        public RegisterCommand(long idCupo, String placa, Date horaEntrada, Date horaSalida, String tipoVehiculo) {
+                this.idCupo = idCupo;
+                this.placa = placa;
+                this.horaEntrada = horaEntrada;
+                this.horaSalida = horaSalida;
+                this.tipoVehiculo = tipoVehiculo;
+        }
+
+        public long getIdCupo() {
+                return this.idCupo;
+        }
+
+        public void setIdCupo(long idCupo) {
+                this.idCupo = idCupo;
+        }
+
+        public String getPlaca() {
+                return this.placa;
+        }
+
+        public void setPlaca(String placa) {
+                this.placa = placa;
+        }
+
+        public Date getHoraEntrada() {
+                return this.horaEntrada;
+        }
+
+        public void setHoraEntrada(Date horaEntrada) {
+                this.horaEntrada = horaEntrada;
+        }
+
+        public Date getHoraSalida() {
+                return this.horaSalida;
+        }
+
+        public void setHoraSalida(Date horaSalida) {
+                this.horaSalida = horaSalida;
+        }
+
+        public String getTipoVehiculo() {
+                return this.tipoVehiculo;
+        }
+
+        public void setTipoVehiculo(String tipoVehiculo) {
+                this.tipoVehiculo = tipoVehiculo;
+        }
+
+}
---
+++ src/main/java/co/com/ceiba/parqueadero/sebastian/parqueadero/aplicacion/command/handler/RegisterHandler.java
@@ -0,0 +1,22 @@
+package co.com.ceiba.parqueadero.sebastian.parqueadero.aplicacion.command.handler;
+
+import co.com.ceiba.parqueadero.sebastian.parqueadero.aplicacion.command.RegisterCommand;
+import co.com.ceiba.parqueadero.sebastian.parqueadero.dominio.modelos.cupos.ModelCupos;
+import co.com.ceiba.parqueadero.sebastian.parqueadero.dominio.servicios.ServicioCupos;
+
+public class RegisterHandler {
+
+    private final ServicioCupos servicioCupos;
+
+    public RegisterHandler(ServicioCupos servicioCupos) {
+        this.servicioCupos = servicioCupos;
+    }
+
+    public void create(RegisterCommand register) {
+        ModelCupos cupo = new ModelCupos(register.getIdCupo(), register.getPlaca(), register.getHoraEntrada(),
+                register.getHoraSalida(), register.getTipoVehiculo());
+
+        this.servicioCupos.create(cupo);
+    }
+
+}
